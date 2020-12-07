cc6903108df66c53c42bb58ad198ec99a0a571c1
Ramsey Nasser <ram@nas.sr>
2020-12-06 19:12:43 -0500

    Initial commit

---
+++ api.js
@@ -0,0 +1,190 @@
+function api(fb, pal) {
+
+    //// math
+    window.lerp = function (x, y, a) { return x * (1 - a) + y * a };
+    window.ilerp = function (x, y, a) { return (a - x) / (y - x) };
+    window.clamp = function (a, min = 0, max = 1) { return Math.min(max, Math.max(min, a)) };
+    window.remap = function (x1, y1, x2, y2, a) { return window.lerp(x2, y2, window.ilerp(x1, y1, a)) };
+    window.abs = Math.abs;
+    window.atan2 = Math.atan2;
+    window.cos = function (x) { return Math.cos(window.remap(0, 1, 0, 2 * Math.PI, x)) };
+    window.sin = function (x) { return Math.sin(window.remap(0, 1, 0, 2 * Math.PI, x)) };
+    window.cls = function (c) {
+        fb.data.fill(c)
+    };
+
+    window.pset = function (xx, yy, c) {
+        let x = Math.trunc(xx);
+        let y = Math.trunc(yy);
+        if (x >= fb.width || y >= fb.height || x < 0 || y < 0)
+            return;
+        fb.data[(x % fb.width) + y * fb.width] = c;
+    };
+    window.pget = function (xx, yy) {
+        let x = Math.trunc(xx);
+        let y = Math.trunc(yy);
+        if (x >= fb.width || y >= fb.height || x < 0 || y < 0)
+            return 0;
+        return fb.data[(x % fb.width) + y * fb.width];
+    };
+
+    // https://answers.unity.com/questions/380035/c-modulus-is-wrong-1.html
+    window.mod = function (a, n) {
+        return ((a % n) + n) % n;
+    };
+
+    // http://members.chello.at/easyfilter/bresenham.html
+    // special thanks to @porglezomp and @prathyvsh
+    window.circ = function (x0, y0, radius, c) {
+        let x = Math.trunc(-radius);
+        let y = 0;
+        let err = Math.trunc(2 - 2 * radius);
+        do {
+            window.pset(x0 - x, y0 + y, c);
+            window.pset(x0 - y, y0 - x, c);
+            window.pset(x0 + x, y0 - y, c);
+            window.pset(x0 + y, y0 + x, c);
+            radius = err;
+            if (radius <= y) err += ++y * 2 + 1;
+            if (radius > x || err > y) err += ++x * 2 + 1;
+        } while (x < 0);
+    };
+
+    // https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+    window.fcirc = function (x0, y0, radius, c) {
+        let x = Math.trunc(-radius);
+        let y = 0;
+        let err = Math.trunc(2 - 2 * radius);
+        let i = 0;
+        do {
+            window.line(x0 - x, y0 + y, x0, y0 + y, c);
+            window.line(x0 - y, y0 - x, x0, y0 - x, c);
+            window.line(x0 + x, y0 - y, x0, y0 - y, c);
+            window.line(x0 + y, y0 + x, x0, y0 + x, c);
+            i++;
+            radius = err;
+            if (radius <= y) err += ++y * 2 + 1;
+            if (radius > x || err > y) err += ++x * 2 + 1;
+        } while (x < 0);
+    };
+
+    // https://stackoverflow.com/questions/11678693/all-cases-covered-bresenhams-line-algorithm
+    window.line = function (x0, y0, x1, y1, c) {
+        if (y0 == y1) {
+            if (x0 < x1) {
+                if (x0 < 0) x0 = 0;
+                if (x1 > fb.width) x1 = fb.width;
+                while (x0 < x1) {
+                    window.pset(x0, y0, c);
+                    x0 += 1;
+                }
+            } else {
+                if (x1 < 0) x1 = 0;
+                if (x0 > fb.width) x0 = fb.width;
+                while (x1 < x0) {
+                    window.pset(x1, y0, c);
+                    x1 += 1;
+                }
+
+                return;
+            }
+        }
+
+        if (x0 == x1) {
+            if (y0 < y1) {
+                if (y0 < 0) y0 = 0;
+                if (y1 > fb.width) y1 = fb.width;
+                while (y0 < y1) {
+                    window.pset(x0, y0, c);
+                    y0 += 1;
+                }
+            } else {
+                if (y1 < 0) y1 = 0;
+                if (y0 > fb.width) y0 = fb.width;
+                while (y1 < y0) {
+                    window.pset(x0, y1, c);
+                    y1 += 1;
+                }
+
+                return;
+            }
+        }
+
+        let w = x1 - x0;
+        let h = y1 - y0;
+        let dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+        if (w < 0) dx1 = -1;
+        else if (w > 0) dx1 = 1;
+        if (h < 0) dy1 = -1;
+        else if (h > 0) dy1 = 1;
+        if (w < 0) dx2 = -1;
+        else if (w > 0) dx2 = 1;
+        let longest = Math.abs(w);
+        let shortest = Math.abs(h);
+        if (!(longest > shortest)) {
+            longest = Math.abs(h);
+            shortest = Math.abs(w);
+            if (h < 0) dy2 = -1;
+            else if (h > 0) dy2 = 1;
+            dx2 = 0;
+        }
+
+        let numerator = longest / 2.0;
+        for (let i = 0; i <= longest; i++) {
+            window.pset(x0, y0, c);
+            numerator += shortest;
+            if (!(numerator < longest)) {
+                numerator -= longest;
+                x0 += dx1;
+                y0 += dy1;
+            }
+            else {
+                x0 += dx2;
+                y0 += dy2;
+            }
+        }
+    };
+
+    window.rect = function (xx1, yy1, xx2, yy2, c) {
+        let x1 = Math.min(xx1, xx2);
+        let y1 = Math.min(yy1, yy2);
+        let x2 = Math.max(xx1, xx2);
+        let y2 = Math.max(yy1, yy2);
+
+        for (let y = y1; y <= y2; y += 1) {
+            window.pset(x1, y, c);
+            window.pset(x2, y, c);
+        }
+
+        for (let x = x1; x <= x2; x += 1) {
+            window.pset(x, y1, c);
+            window.pset(x, y2, c);
+        }
+    };
+
+    window.frect = function (xx1, yy1, xx2, yy2, c) {
+        let x1 = Math.min(xx1, xx2);
+        let y1 = Math.min(yy1, yy2);
+        let x2 = Math.max(xx1, xx2);
+        let y2 = Math.max(yy1, yy2);
+
+        for (let y = y1; y <= y2; y += 1) {
+            for (let x = x1; x <= x2; x += 1) {
+                window.pset(x, y, c);
+            }
+        }
+    };
+
+    window.rrow = function (row, distance) {
+        if (distance > 0) {
+            for (let i = 0; i < fb.width; i++) {
+                window.pset(i, row, window.pget(window.mod(Math.trunc(i + distance), Math.trunc(fb.width)), row));
+            }
+        }
+        else {
+            for (let i = Math.trunc(fb.width); i >= 0; i--) {
+                window.pset(i, row, window.pget(window.mod(Math.trunc(i + distance), Math.trunc(fb.width)), row));
+            }
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,225 @@
+// based on https://gist.github.com/strangerintheq/27b8fc4e53432d8b9284364713ce8608
+
+const vertexShader = `#version 300 es
+
+precision highp float;
+
+void main(void) {
+    float x = float((gl_VertexID & 1) << 2);
+    float y = float((gl_VertexID & 2) << 1);
+    gl_Position = vec4(x - 1.0, y - 1.0, 0, 1);
+}
+`
+
+const fragmentShader = `#version 300 es
+
+precision highp float;
+precision highp int;
+precision highp usampler2D;
+
+uniform usampler2D framebuffer;
+uniform sampler2D palette;
+uniform vec2 screen;
+uniform float paletteSize;
+
+out vec4 color;
+
+void main(void) {
+    float smallest_side = min(screen.y, screen.x);
+    float biggest_side = max(screen.y, screen.x);
+    float difference = biggest_side - smallest_side;
+    vec2 padding = vec2(0.0);
+    if(screen.x <= screen.y)
+        padding.y = difference / 2.;
+    else
+        padding.x = difference / 2.;
+    float ix = (gl_FragCoord.x - padding.x) / smallest_side;
+    float iy = (gl_FragCoord.y - padding.y) / smallest_side;
+    if(ix < 0. || ix >= 1. || iy < 0. || iy >= 1.) {
+        color = texture(palette, vec2(0));
+    } else {
+        uint idx = texture(framebuffer, vec2(ix, iy)).r;
+        color = texture(palette, vec2(idx, 0) / paletteSize);
+    }
+}
+`
+
+// dom setup
+let canvas = document.body.appendChild(document.createElement('canvas'));
+
+let textarea = document.createElement('textarea')
+document.body.appendChild(textarea)
+var editor = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: false,
+    theme: "liquibyte"
+  });
+
+editor.on('change', e => {
+    let compiled = wisp.compile(editor.getDoc().getValue())
+    if(compiled.code) {
+        console.log(compiled.code);
+        draw = new Function(['t', 'f'], compiled.code)
+    } else {
+        console.error(compiled)
+    }
+})
+
+window.onload = function() {
+    editor.setValue(`;; T H I X E L S
+;; no documentation for now :(
+
+(cyc 128 nil (cls (cyc 64 3 4 0)))
+
+(defn ice-cream [x y r]
+  (forl [i 0 (* 3 r)]
+        (line (+ i (- x (* 2 r))) y x (- y (* 3 r)) 8)
+        (line (- (+ x (* 2 r)) i) y x (- y (* 3 r)) 9))
+  (fcirc (+ x (/ r 1)) y r 4)
+  (fcirc x (+ y r) r 14)
+  (fcirc (- x (/ r 1)) y r 6))
+
+(forl [i 0 32]
+      (let [rad 0.05
+            spd 0.01
+            scl 0.005
+            len 2048
+            aspd 0.5
+            tt (mod (* 64 (+ i (* aspd t))) len)]
+        (ice-cream (+ 64 (* rad tt (cos (* spd tt))))
+                   (+ 64 (* rad tt (sin (* spd tt))))
+                   (* scl tt))))
+
+(cyc 256
+     nil
+     (forl [i 0 128]
+           (rrow i (sin (+ t i)))))
+`)
+    
+    
+    console.log(editor);
+}
+
+// gl setup
+let gl = canvas.getContext('webgl2');
+gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+
+// shader setup
+let program = createProgram(vertexShader, fragmentShader);
+gl.useProgram(program);
+
+window.onresize = function () {
+    gl.viewport(0, 0, canvas.width = window.innerWidth, canvas.height = window.innerHeight);
+    gl.uniform2f(gl.getUniformLocation(program, "screen"), canvas.width, canvas.height);
+}
+window.onresize()
+
+var framebuffer = new Framebuffer(128, 128)
+var palette = new Palette("https://lospec.com/palette-list/pico-8.json")
+api(framebuffer, palette)
+
+function draw(t, f) {
+    
+}
+
+let f = 0
+function render(now = 0) {
+    now *= 0.0001
+    requestAnimationFrame(render)
+    draw(now, f++)
+    framebuffer.update()
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 3);
+}
+
+function Framebuffer(width, height) {
+    this.width = width
+    this.height = height
+    this.data = new Uint8Array(width * height)
+    this.unit = 0
+    this.update = function () {
+        gl.activeTexture(gl.TEXTURE0 + this.unit);
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, this.width, this.height, gl.RED_INTEGER, gl.UNSIGNED_BYTE, this.data)
+    }
+
+    makeTexture(width, height, this.unit, gl.R8UI, gl.RED_INTEGER, gl.UNSIGNED_BYTE, this.data)
+    gl.uniform1i(gl.getUniformLocation(program, "framebuffer"), this.unit)
+
+}
+
+function Palette(init) {
+    this.unit = 1
+    this.update = function () {
+        gl.activeTexture(gl.TEXTURE0 + this.unit);
+        gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, this.size, 1, gl.RGB, gl.UNSIGNED_BYTE, this.data)
+    }
+    this.resize = function (newSize) {
+        this.size = newSize
+        this.data = new Uint8Array(this.size * 3)
+        makeTexture(this.size, 1, this.unit, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, this.data)
+        gl.uniform1i(gl.getUniformLocation(program, "palette"), this.unit)
+        gl.uniform1f(gl.getUniformLocation(program, "paletteSize"), this.size)
+    }
+    this.populateFromData = function (data) {
+        console.log(`loaded palette ${data.name} by ${data.author}`);
+        this.resize(data.colors.length)
+        let i = 0;
+        for (const color of data.colors) {
+            let r = parseInt(color.substr(0, 2), 16)
+            let g = parseInt(color.substr(2, 2), 16)
+            let b = parseInt(color.substr(4, 2), 16)
+            this.data[i + 0] = r
+            this.data[i + 1] = g
+            this.data[i + 2] = b
+            i += 3
+        }
+        this.update()
+    }
+    this.populateFromUrl = function (url) {
+        const key = `thixels:palette${url}`
+        if (localStorage.getItem(key))
+            return this.populateFromData(JSON.parse(localStorage.getItem(key)))
+        fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+            .then(response => response.json())
+            .then(data => {
+                localStorage.setItem(key, JSON.stringify(data))
+                this.populateFromData(data)
+            });
+    }
+    if (typeof init == 'number')
+        this.resize(size)
+    else if (typeof init == 'string')
+        this.populateFromUrl(init)
+}
+
+function makeTexture(width, height, unit, internalFormat, format, type, data) {
+    let texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0 + unit);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, width, height, 0,
+        format, type, data);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+    return texture;
+}
+
+function createShader(source, type) {
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+        throw new Error(gl.getShaderInfoLog(shader));
+    return shader;
+}
+
+function createProgram(vertex, fragment) {
+    let program = gl.createProgram();
+    gl.attachShader(program, createShader(vertex, gl.VERTEX_SHADER));
+    gl.attachShader(program, createShader(fragment, gl.FRAGMENT_SHADER));
+    gl.linkProgram(program);
+
+    return program;
+}
+
+render();
